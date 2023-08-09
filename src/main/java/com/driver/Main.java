df7d0d8430937b1f467a0cc54d451852b9201bc2
Kulbir Singh Bhandari <kulbirbhandari04@gmail.com>
2023-08-09 15:57:02 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+//        r.name = "Ashish";
+//        System.out.println(r.name);
+        r.setter("Kulbir");
+        System.out.println(r.getter());
+//        java: name has private access in com.driver.RWOnly
+    }
 }
